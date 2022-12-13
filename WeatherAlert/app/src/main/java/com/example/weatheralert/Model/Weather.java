8fc54d311a4a80f48919d03d73788a9c9e63a36f
Gabrel Rodrigues <gabriel02aguiar@gmail.com>
2022-12-13 17:02:41 -0300

    androidCommit

---
+++ WeatherAlert/app/src/main/java/com/example/weatheralert/Model/Weather.java
@@ -0,0 +1,35 @@
+package com.example.weatheralert.Model;
+
+public class Weather {
+
+    private int cod;
+    private int weather;
+
+    public Weather(){
+
+    }
+
+    public Weather(int cod, int weather){
+        this.cod = cod;
+        this.weather = weather;
+    }
+
+    public Weather(int cod, String string) {
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public int getWeather() {
+        return weather;
+    }
+
+    public void setWeather(int weather) {
+        this.weather = weather;
+    }
+}
